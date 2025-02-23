84dbdcb1722fc71791dd3585e0d2fe9d086537d7
İsa Öcel <isaocel@gmail.com>
2025-02-23 21:48:04 +0300

    ilk test tamamlandı

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,5 +1,18 @@
-describe("template spec", () => {
+describe("İlk Ziyaret", () => {
   it("passes", () => {
     cy.visit("http://localhost:5173");
+  });
+});
+
+describe("Başarılı Form Gönderimi", () => {
+  it("Form başarıyla gönderildiğinde kullanıcı ID gözüküyor", () => {
+    cy.visit("http://localhost:5173");
+    cy.get("#ad").type("İsa");
+    cy.get("#soyad").type("Öcel");
+    cy.get("#email").type("isaocel@gmail.com");
+    cy.get("#password").type("Password123!.?");
+    cy.get(".submit-button").click();
+
+    cy.get(".card-footer").should("be.visible").contains("ID:");
   });
 });
